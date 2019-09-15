3d17fe9d4d4b87c3372c91d22f064b4e36525fdc
prakarsh <prakarshparashar@gmail.com>
2019-09-15 10:18:27 +0530

    Final working on loopback interface

--- chat_app_p2p/server_app.py
+++ chat_app_p2p/server_app.py
@@ -2,53 +2,37 @@ import socket
 import select
 import sys
 
-class passive_socket(socket.socket):
 
-    def __init__(self, sock=None):
-        if sock is None:
-            self.sock = super().__init__(socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-
-class active_socket(socket.socket):
-
-    def __init__(self, sock=None, client_addr=None):
-        if sock is None:
-#            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.sock = super().__init__(socket.AF_INET, socket.SOCK_STREAM)
-        else:
-            self.sock = sock
-
-    def chat(self):
-        
-        inputs = [self.sock, sys.stdin]
-        #chat starts
-        while True:
-            readers, _, _ = select.select([self.sock, sys.stdin], [], [])
-            msg = ""
-            for reader in readers:
-                if reader is self.sock:
-                    msg = self.sock.recv(BUF_SIZE).decode('utf8')
-                    if msg != "!q":
-                        print(f"<<Friend>> {msg}")
-                    else:
-                        print(f"Friend closed connection")
-                        self.sock.close()
+def chat(sock):
+    
+    inputs = [sock, sys.stdin]
+    #chat starts
+    while True:
+        readers, _, _ = select.select([sock, sys.stdin], [], [])
+        msg = ""
+        for reader in readers:
+            if reader is sock:
+                msg = sock.recv(BUFSIZE).decode('utf8')
+                if msg != "!q":
+                    print(f"<<Friend>> {msg}")
                 else:
-                    msg = sys.stdin.readline()
-                    if msg != "!q":
-                        print(f"<<Me>> {msg}")
-                        self.sock.send(bytes(msg, 'utf-8'))
-                    else:
-                        self.sock.send(bytes(msg, 'utf-8'))
-                        print(f"Closing connection")
-                        self.sock.close()
+                    print(f"Friend closed connection")
+                    sock.close()
+            else:
+                msg = sys.stdin.readline().rstrip()
+                if msg != "!q":
+                    print(f"<<Me>> {msg}")
+                    sock.send(bytes(msg, 'utf-8'))
+                else:
+                    sock.send(bytes(msg, 'utf-8'))
+                    print(f"Closing connection")
+                    sock.close()
 
             if msg == "!q":
                 break
 
-
-
+        if msg == "!q":
+            break
 
 
 BUFSIZE = 1024
@@ -59,7 +43,7 @@ if __name__ == "__main__":
     ADDR1 = (HOST, PORT1)
     ADDR2 = (HOST, PORT2)
 
-    listen_sock = passive_socket()
+    listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     listen_sock.bind(ADDR1)
     listen_sock.listen(5)
     
@@ -71,20 +55,21 @@ if __name__ == "__main__":
         readers, _, _ = select.select(inputs, [], [])
         for reader in readers:
             if reader is listen_sock:
-                conn_sock, client_addr = listen_sock.accept()
-                chat_sock_serv = active_socket(conn_sock, client_addr)
+                print("I am listen_socket")
+                chat_sock_serv, client_addr = listen_sock.accept()
                 app = "server"
                 break
             else:
-                chat_sock_client = active_socket()
+                print("I am stdin")
+                chat_sock_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
                 chat_sock_client.connect(ADDR2)
                 app = "client"
                 break
 
         if app == "server":
-            chat_sock_serv.chat()
+            chat(chat_sock_serv)
             del chat_sock_serv
         elif app == "client":
-            chat_sock_client.chat()
+            chat(chat_sock_client)
             del chat_sock_client
     
